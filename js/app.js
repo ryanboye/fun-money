a1079f9ec8b5cef4c5a3f96494104b9a04c6be80
Ryan Boye <rboye@cloudflare.com>
2014-07-26 21:15:38 -0700

    Adding js dependencies, the index loads are bork

--- js/app.js
+++ js/app.js
@@ -16,18 +16,44 @@ var PeriodView = Marionette.ItemView.extend({
     template: "#periodView" 
 });
 
+
+var Today = Backbone.Model.extend({
+	name: "Today",
+	date: "July 24, 2014",
+	spent: 16,
+	budget: 50,
+	profit: budget - spent,
+});
+var today = new Today();
+console.log(today);
+
+var Week = Backbone.Model.extend({
+	name: "This Week",
+	date: "July 24, 2014",
+	spent: 16,
+	budget: 50,
+	profit: budget - spent,
+});
+var week = new Week();
+
+var Month = Backbone.Model.extend({
+	name: "This Month",
+	date: "July 24, 2014",
+	spent: 16,
+	budget: 50,
+	profit: budget - spent,
+});
+var month = new Month();
+
 // Initialize
-
 var todayView = new TodayView({
-	model: today;
+	model: today
 });
-
 var weekView = new WeekView({
-	model: week;
+	model: week
 });
-
 var monthView = new MonthView({
-	model: month;
+	model: month
 });
 
 App.today.show(todayView);
