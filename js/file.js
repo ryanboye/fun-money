0a07a912b494563637bcddf1a282150ce244d3bf
VoX <icopy11@gmail.com>
2014-07-27 01:15:59 -0400

    added file selector

--- js/file.js
+++ js/file.js
@@ -1,19 +1,8 @@
-function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
+  function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
 
-    var files = evt.dataTransfer.files; // FileList object.
-    MINT.parse(files[0]);
+    MINT.parse(files[0], function(){ console.log("loaded")});
+  }
 
-}
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-function handleDragOver(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-}
-
-// Setup the dnd listeners.
-var dropZone = document.getElementById('main');
-dropZone.addEventListener('dragover', handleDragOver, false);
-dropZone.addEventListener('drop', handleFileSelect, false);
